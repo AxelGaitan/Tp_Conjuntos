b15c78324458a987b407e898bc62fe2057115f9a
Axel Gaitan <agaitan44@yahoo.com>
2025-05-25 21:10:19 -0300

    Agrego script

---
+++ conjuntos.py
@@ -0,0 +1,36 @@
+#Operaciones con el dni ingresado del usuario
+
+
+
+
+
+
+#Generar conjuntos de digitos
+
+
+
+
+
+
+#Conteo de frecuencia de cada digito por DNI
+
+
+
+
+
+
+#Suma total de los digitos de cada DNI.
+
+
+#Evaluacion de condiciones
+
+
+
+
+
+
+#OPERACION CON LOS AÑOS
+
+
+
+
